58c573e466980a9464df1c41f531b984e5ca7e34
Sergio Gimenez <sergiogimenezanton@gmail.com>
2022-03-05 11:08:09 +0100

    Added line breaks in network_test

--- network_test.sh
+++ network_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Simple script for checking if the hosts are up and running
 
-hosts=("127.0.0.3")
+hosts=("127.0.0.3", "123.123.123.123", "127.0.0.1")
 
 for host in "${hosts[@]}"; do
     ping -c 1 $host -w 2 > /dev/null
 
     if [ $? -eq 0 ]; then
-        printf "\xE2\x9C\x94 $host is up"
+        printf "\xE2\x9C\x94 $host is up\n"
     else
-        printf "\xE2\x9D\x8C $host is down"
+        printf "\xE2\x9D\x8C $host is down\n"
     fi
 
 done
